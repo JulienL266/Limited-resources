1f378de4b8ae5fca3286b325499ee02bd9d7615c
JulienL266 <julien.laurendeau@epfl.ch>
2023-10-31 10:30:28 +0100

    Update icu_interference.R

--- icu_interference.R
+++ icu_interference.R
@@ -1,4 +1,11 @@
+#reading icu data
 library(haven)
 data <- read_dta("~/Downloads/icu_pseudo_data.dta")
 set.seed(2023)
 n <- nrow(data)
+
+#implementing 2016 Luedtke and van der Laan algo
+library(SuperLearner)
+#estimating Q_0
+
+#estimating Q_{b,o}
