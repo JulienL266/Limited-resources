e0a02859154cc4ce1f085f0d52544476e9edf26c
JulienL266 <julien.laurendeau@epfl.ch>
2023-11-02 13:47:33 +0100

    Update icu_interference.R

--- icu_interference.R
+++ icu_interference.R
@@ -13,9 +13,9 @@ A <- data$icu_bed
 data <- select(data, !c(icu_bed))
 
 ## define kappa (might change later)
-#kappa <- mean(A)
+kappa <- mean(A)
 #kappa <- 0.2
-kappa <- 0.6
+#kappa <- 0.6
 #kappa <- 0.05
 
 ## Removing uninteresting variables
