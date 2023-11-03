ab8b35c75e7dfcd4f535eaca6b5a638dca264a5a
JulienL266 <julien.laurendeau@epfl.ch>
2023-11-03 14:15:28 +0100

    Update icu_interference_online.R

--- icu_interference_online.R
+++ icu_interference_online.R
@@ -25,7 +25,8 @@ data <- select(data, !c(id))
 L <- data[,c("age", "male", "sofa_score", "open_beds_cmp")]
 
 #Estimation 
-D_tilde <- function(d, Q, g, y,a,w){
-  D_1 <- ((a*d(w) + (1-a)*(1-d(w)))/(a*g(1,w) + (1-a)*g(0,w)))*(y - Q(a,w))
-  return(D_1 + Q(d(w),w))
+D_tilde <- function(d, Q, g, y,a,w,j){
+  D_1 <- ((a*d(w,j) + (1-a)*(1-d(w,j)))/(a*g(1,w,j) + (1-a)*g(0,w,j)))*(y - Q(a,w,j))
+  return(D_1 + Q(d(w,j),w,j))
 }
+
