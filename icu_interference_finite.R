2c54c9524d33e82cb76f3ddc4a095a19699cf32a
JulienL266 <julien.laurendeau@epfl.ch>
2023-11-21 15:46:29 +0100

    Update icu_interference_finite.R

---
+++ icu_interference_finite.R
@@ -0,0 +1,27 @@
+# Reading icu data (semi-parametric estimators)
+library(haven)
+library(dplyr)
+data <- read_dta("~/Downloads/icu_pseudo_data.dta")
+set.seed(2023)
+n <- nrow(data)
+
+# Defining variables
+Y <- (1 - data$dead90)
+data <- select(data, !c(dead7, dead28, dead90))
+
+A <- data$icu_accept
+data <- select(data, !c(icu_accept))
+
+## define kappa (might change later)
+kappa <- mean(A)/2
+#kappa <- mean(A)
+#kappa <- mean(data$icu_recommend)
+#kappa <- 1
+
+## Removing uninteresting variables
+data <- select(data, !c(id))
+
+## Categorizing sofa score as in the guidelines
+
+#chosen variables, may change, follows Wang, Qi and Shi (2022)
+L <- data[,c("age", "male", "sofa_score")]
