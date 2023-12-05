be4613e414ee24c292e0377c1b60e082516e7364
JulienL266 <julien.laurendeau@epfl.ch>
2023-12-05 20:36:30 +0100

    Update icu_interference_finite.R

--- icu_interference_finite.R
+++ icu_interference_finite.R
@@ -184,7 +184,7 @@ for(i in 1:n){
   }
   #Val.IPW <- Val.IPW + Y[i]*q_star(A[i], L[i,])/(n*q_n(A[i], L[i,]))
   #saturated try-outs
-  Val.IPW <- Val.IPW + Y[i]*A[i]*q_star(A[i], L[i,])/(n*q_n(A[i], L[i,]))
+  Val.IPW <- Val.IPW + Y[i]*q_star(A[i], L[i,])/(n*q_n(A[i], L[i,]))
   setTxtProgressBar(pb,i)
 }
 Val.IPW
