f54fe0598b8b718613e7c9eb5edfc41fd25c3d66
JulienL266 <julien.laurendeau@epfl.ch>
2023-12-13 17:44:42 +0100

    Update icu_interference_online.R

--- icu_interference_online.R
+++ icu_interference_online.R
@@ -137,10 +137,11 @@ D_n <- function(y,a,w,j){
 }
 
 sigma_n <- function(j){
-  res <- c()
-  for(i in l_n:(j-1)){
-    res <- c(res, D_n(Y[i],A[i],L[i,],j))
-  }
+  #res <- c()
+  #for(i in l_n:(j-1)){
+   # res <- c(res, D_n(Y[i],A[i],L[i,],j))
+  #}
+  res <- D_n(Y[l_n:(j-1)], A[l_n:(j-1)], L[l_n:(j-1),],j)
   return(sd(res))
 }
 
